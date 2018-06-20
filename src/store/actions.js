c464803b35861aedf32fc7fe73a314862451be17
DESKTOP-JTP554M\User <nikolayev.sergii@gmail.com>
2018-06-20 16:59:29 +0300

    Add Input, Output, List, store
    important files

---
+++ src/store/actions.js
@@ -0,0 +1,6 @@
+export const ADD_ITEM = 'ADD_ITEM';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const FILTER_ALL = 'FILTER_ALL';
+export const FILTER_DONE = 'FILTER_DONE';
+export const FILTER_UNDONE = 'FILTER_UNDONE';
+
